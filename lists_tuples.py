e196ba0ce664486b7c62922fa80ec0508fc03526
Mohil Garg <112541468+mohilcode@users.noreply.github.com>
2023-07-27 00:33:41 +0900

    Create lists_tuples.py

---
+++ lists_tuples.py
@@ -0,0 +1,9 @@
+courses = ['History', 'math', 'physics', 'comp sci']
+print(courses)
+print(len(courses))
+print(courses[0])
+print(courses[len(courses) - 1])
+print(courses[-1])
+print(courses[0:2])
+print(courses[:2])
+print(courses[0:])
