8c6108fb44e0930a9f2cc61b7fe0a719edb5b03e
Mohil Garg <112541468+mohilcode@users.noreply.github.com>
2023-07-27 00:27:49 +0900

    Create integers_floats.py

---
+++ integers_floats.py
@@ -0,0 +1,18 @@
+num = 3
+print(type(num))
+print(3+2)
+print(3 / 2)
+print(3 // 2)
+print(3 ** 2)
+print(3 % 2)
+print(3 * 2 + 1)
+print(3 * (2 + 1))
+print(abs(-3))
+print(round(3.75))
+print(round(3.75, 1))
+print(3 < 2)
+
+num_1 = '200'
+num_2 = '100'
+print(int(num_1) + int(num_2)) 
+
